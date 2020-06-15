8210ae9d8dfea495d9d3d9116e032d4543773f42
Jatin <jatinmadaan9882@gmail.com>
2020-06-15 17:18:29 +0530

    Authentication done

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('register_user/', views.register_user, name='register_user')
 ]
--- students/views.py
+++ students/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib import messages
+from .models import User
+
 
 # Create your views here.
 
 def login(request):
-    context = {}
-    return render(request, 'studentbook/login.html', context)
+    if request.POST:
+        email = request.POST['email']
+        password = request.POST['password']
+
+        count = User.objects.filter(email=email, password=password).count()
+        if count > 0 :
+            return HttpResponse("You are authenticated successfully ...")
+        else:
+            messages.error(request, 'Invalid email or password')
+            return redirect('login')
+    return render(request, 'studentbook/login.html')
 
 
 def signup(request):
     context = {}
     return render(request, 'studentbook/signup.html',context)
+
+def register_user(request):
+
+    if request.POST:
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        obj = User(username=username, password = password, email = email)
+        obj.save()
+        messages.success(request, 'You are registered successfully .. ')
+        return redirect('login')

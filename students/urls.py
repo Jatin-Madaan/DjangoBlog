4b8d6d151f6eb0f74f8efcc9928d2d950a54816d
Jatin <jatinmadaan9882@gmail.com>
2020-06-15 17:49:42 +0530

    logout functionality using Session

--- students/urls.py
+++ students/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('register_user/', views.register_user, name='register_user')
+    path('register_user/', views.register_user, name='register_user'),
+    path('home/', views.home, name='home'),
+    path('logout/', views.logout, name='logout')
 ]
--- students/views.py
+++ students/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.sessions.models import Session
 from .models import User
 
 
 # Create your views here.
 
 def login(request):
+    if request.session.has_key('is_logged'):
+        return redirect('home')
+
     if request.POST:
         email = request.POST['email']
         password = request.POST['password']
 
         count = User.objects.filter(email=email, password=password).count()
         if count > 0 :
-            return HttpResponse("You are authenticated successfully ...")
+            request.session['is_logged'] = True
+            return redirect('home')
         else:
             messages.error(request, 'Invalid email or password')
             return redirect('login')
@@ -35,3 +40,12 @@ def register_user(request):
         obj.save()
         messages.success(request, 'You are registered successfully .. ')
         return redirect('login')
+
+def home(request):
+    if request.session.has_key('is_logged'):
+        return render(request, 'studentbook/home.html')
+    return redirect('login')
+
+def logout(request):
+    del request.session['is_logged']
+    return redirect('login')
